6f855b8debbe9681ef969ef585ec7cb7ad1f2019
JamesM134 <114939043+JamesM134@users.noreply.github.com>
2022-12-01 11:58:18 -0500

    Create Main.java

---
+++ 02-fizzbuzz/Main.java
@@ -0,0 +1,26 @@
+ class Main{  
+public static void main(String args[])   
+{  
+System.out.println("The Fizz, Buzz, and FizzBuzz numbers are: ");  
+for (int i = 1; i <= ; i++)   
+{  
+if (i%3==0 && i%5==0)   
+{   
+System.out.print("FizzBuzz");  
+}   
+else if (i%3==0)   
+{  
+System.out.print("Fizz");  
+}   
+else if (i%5==0)   
+{   
+System.out.print("Buzz");  
+}   
+else   
+{  
+System.out.print(i);  
+}  
+System.out.print(","+" ");  
+    }  
+  }  
+}  
