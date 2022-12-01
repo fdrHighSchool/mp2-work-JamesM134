aacd63b71673a05b5a769a9c5e22d9b8e296d05d
JamesM134 <114939043+JamesM134@users.noreply.github.com>
2022-12-01 12:04:43 -0500

    Create Main.java

---
+++ 06-shapes/Main.java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(String[] args) {
+  sentence(6,4);
+  }
+  public static void sentence(int l , int w){
+    for(int row = 0; row < w; row ++){
+    for(int col = 0; col < l; col++){
+      System.out.print("*");
+  }
+    System.out.println();
+    }
+  }
+}
+____________________________________________________
+
+class Main{
+  public static void main (String[] args){
+    rightTriangle(4);
+  }
+  public static void rightTriangle(int s){
+    int p = 0;
+    for(int row=0; )
+    for(int col=0; col <=p; col++){
+      
+    }
+  }
+}
